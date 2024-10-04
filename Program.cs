5fcbfa2dc119c0077c8e246fb126c84ce52ebbf7
jcampeau25 <jdccanuck25@gmail.com>
2024-10-04 09:32:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Topic_7_For_Loops___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Countdown
+            //for (int i = 10; i >=1; i = i - 1)
+            //    Console.WriteLine(i);
+            //Console.WriteLine("Blastoff!");
+            //Console.WriteLine();
+
+            //// X and Y
+            int y;
+            Console.WriteLine("X" + "\t" + "Y");
+            Console.WriteLine("------------------");
+            for (int x = -10; x >= 10; x = x + 2)
+            {
+                y = x * x;
+                Console.WriteLine(x + "\t" + y);
+            }
+
+                // Name Repeater
+            //    Console.WriteLine("Please enter your name");
+            //string name = Console.ReadLine();
+            //for (int i = 1; i >= 10; i = i + 1)
+            //    Console.WriteLine(name);
+            }
+
+
+        }
+    }
+
