63c2950f424087870f6c47031d9da417bfe1b5b6
jcampeau25 <jdccanuck25@gmail.com>
2024-10-07 09:28:11 -0400

    finished

--- Program.cs
+++ Program.cs
@@ -5,27 +5,37 @@
         static void Main(string[] args)
         {
             //// Countdown
-            //for (int i = 10; i >=1; i = i - 1)
-            //    Console.WriteLine(i);
-            //Console.WriteLine("Blastoff!");
-            //Console.WriteLine();
+            for (int i = 10; i >= 1; i = i - 1)
+                Console.WriteLine(i);
+            Console.WriteLine("Blastoff!");
+            Console.WriteLine();
 
             //// X and Y
             int y;
             Console.WriteLine("X" + "\t" + "Y");
             Console.WriteLine("------------------");
-            for (int x = -10; x >= 10; x = x + 2)
+            for (int x = -10; x <= 10; x = x + 2)
             {
                 y = x * x;
                 Console.WriteLine(x + "\t" + y);
-            }
 
-                // Name Repeater
-            //    Console.WriteLine("Please enter your name");
-            //string name = Console.ReadLine();
-            //for (int i = 1; i >= 10; i = i + 1)
-            //    Console.WriteLine(name);
             }
+            Console.WriteLine();
+
+            // Name Repeater
+            Console.WriteLine("Please enter your name");
+            int n = 1;
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            if (name.ToLower() == "aldworth")
+                n = 6;
+            for (n = n; n <= 10; n = n + 1)
+                Console.WriteLine(name);
+
+
+
+
+        }
 
 
         }
